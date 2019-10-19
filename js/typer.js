e9e34441577b189283e5583d44bfbfe777148252
SVR666 <sreerajvr24@gmail.com>
2019-10-19 19:06:46 +0530

    initial stage

---
+++ js/typer.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typer', {                              //typer is the class name
+            strings: ["I Love Programming .","I'm An Everday Learner .","I'm Just A Dreamer !"],
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 110,
+            backDelay: 1000,
+            } );
