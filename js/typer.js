2f61d266a327cc095c7578a1416b533152ffa117
SVR666 <sreerajvr24@gmail.com>
2019-10-22 21:34:38 +0530

    css & index.html update

--- js/typer.js
+++ js/typer.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typer', {                              //typer is the class name
-            strings: ["I Love Programming .","I'm An Everday Learner .","I'm Just A Dreamer !"],
+            strings: ["I Love Programming.","I'm An Everday Learner.","I'm Just A Dreamer !"],
             loop: true,
             typeSpeed: 100,
             backSpeed: 110,
